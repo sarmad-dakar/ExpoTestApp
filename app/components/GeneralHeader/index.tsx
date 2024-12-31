cc65670c929133e88049b408d17b11479226f655
sarmad-dakar <sshakeel@dakarsoftware.com>
2024-12-31 02:32:00 -0800

    minor balance related issue fixes

--- app/components/GeneralHeader/index.tsx
+++ app/components/GeneralHeader/index.tsx
@@ -29,9 +29,10 @@ type headerProps = {
     icon: ImageProps;
   };
   color?: string;
+  disable?: boolean
 };
 
-const GeneralHeader = ({ title, back, sport, color }: headerProps) => {
+const GeneralHeader = ({ title, back, sport, color , disable }: headerProps) => {
   const balance = useSelector((state: any) => state.account.balance);
   const topupConfirmationRef = useRef<ConfirmationPopupRef>(null);
   const styles = MyStyles();
@@ -40,6 +41,16 @@ const GeneralHeader = ({ title, back, sport, color }: headerProps) => {
   const handlePress = () => {
     topupConfirmationRef.current?.show();
   };
+
+  const showBalance = ()=> { 
+    if (disable){
+      return false 
+    }
+    if (balance){  
+      return true 
+    }
+    return false 
+  }
 
   return (
     <View style={[styles.container, color ? { backgroundColor: color } : null]}>
@@ -76,7 +87,7 @@ const GeneralHeader = ({ title, back, sport, color }: headerProps) => {
         </View>
       )}
       <BerlingskeMedium style={styles.selectedSport}>{title}</BerlingskeMedium>
-      {balance ? (
+      {showBalance() ? (
         <Pressable
           onPress={handlePress}
           style={{
