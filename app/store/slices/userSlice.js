b1e6963adbb90eaa2c771ca3cc8cef6f2a6d068e
sarmad-dakar <158047476+sarmad-dakar@users.noreply.github.com>
2025-01-30 18:26:30 +0500

    token changes

--- app/store/slices/userSlice.js
+++ app/store/slices/userSlice.js
@@ -39,10 +39,11 @@ const user = createSlice({
   initialState,
   reducers: {
     logout: (state) => {
-      const removeMultipleUser = state.multipleUsers.filter(
-        (item) => item?.user?.token !== state.token
-      );
-
+      const removeMultipleUser = state.multipleUsers.filter((item) => {
+        console.log(item?.user?.name, state.user.name, "testtt");
+        return item?.user?.token !== state.user.token;
+      });
+      console.log(removeMultipleUser, "multiple users");
       state.multipleUsers = removeMultipleUser;
       state.user = null;
       state.token = null;
@@ -67,11 +68,25 @@ const user = createSlice({
     });
     builder.addCase(fetchuserProfile.fulfilled, (state, action) => {
       state.user = action.payload.data;
+      const updateWithTokens = state.multipleUsers.map((item) => {
+        if (item.user?.token == state.user.token) {
+          item.user.token = action.payload.data.token;
+        }
+        return item;
+      });
+      state.multipleUsers = updateWithTokens;
       state.token = action.payload.data?.token;
     });
     builder.addCase(fetchnewToken.fulfilled, (state, action) => {
       console.log(JSON.stringify(action.payload), "api Payload");
       state.user = action.payload.data;
+      const updateWithTokens = state.multipleUsers.map((item) => {
+        if (item.user?.token == state.user.token) {
+          item.user.token = action.payload.data.token;
+        }
+        return item;
+      });
+      state.multipleUsers = updateWithTokens;
       state.token = action.payload.data?.token;
     });
   },
