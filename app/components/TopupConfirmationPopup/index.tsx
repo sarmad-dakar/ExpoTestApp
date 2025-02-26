fe1dbfce52c834368daf6548b702dc4ee6c83a75
sarmad-dakar <158047476+sarmad-dakar@users.noreply.github.com>
2025-02-26 19:46:48 +0500

    key change

--- app/components/TopupConfirmationPopup/index.tsx
+++ app/components/TopupConfirmationPopup/index.tsx
@@ -137,7 +137,7 @@ const TopupConfirmationPopup = forwardRef<
       setLocalLoader(true);
       let data = {
         Amount: topupAmount,
-        IsMobile: 1,
+        RequestMediumId: 1,
         Comment: "",
         PinCode: "",
       };
